3a0c65ee9d8e842a8bca5ae7c2bc7f66aba21656
Dhruv707 <khatri.dhruv07@gmail.com>
2024-03-02 01:22:04 +0530

    cart starting

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -3,13 +3,11 @@ import all_product from "../Components/Assets/all_product";
 
 export const ShopContext = createContext(null);
 
-const ShopContextProvider = (props) => {
+const ShopContextProvider = ({ children }) => {
   const contextValue = { all_product }; //this will be used in all places to give access to all products
 
   return (
-    <ShopContext.Provider value={contextValue}>
-      {props.children}
-    </ShopContext.Provider>
+    <ShopContext.Provider value={contextValue}>{children}</ShopContext.Provider>
   );
 };
 
